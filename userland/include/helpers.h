c4300b9264cb795f301fe599cb7b4ecf960361c7
Markus Sommer <msommer@informatik.uni-marburg.de>
2021-04-29 17:50:53 +0200

    Documentation

--- userland/include/helpers.h
+++ userland/include/helpers.h
@@ -3,9 +3,24 @@
 
 #include "btp.h"
 
+/**
+ * Very simple hash function
+ */
 uint32_t gen_tree_id(unsigned char *str);
+
+/**
+ * 
+ */
 void hexdump(const void* data, size_t size);
+
+/**
+ * Prints MAC address, since doing it by hand is very bothersome...
+ */
 void print_mac(uint8_t * addr);
+
+/**
+ * Pretty-print a BTP-frame
+ */
 void pprint_frame(btp_frame_t *in_frame);
 
 #endif // __HELPERS_H__
--- userland/include/tree.h
+++ userland/include/tree.h
@@ -7,29 +7,41 @@
 
 #define BREADTH 10
 
+/**
+ * State of our parent
+ */
 typedef struct {
     mac_addr_t addr;
-    uint32_t high_pwr;
-    uint32_t snd_high_pwr;
-    uint32_t own_pwr;
+    uint32_t high_pwr; // the power at which the parent does currently broadcast data frames
+    uint32_t snd_high_pwr; // the power at which the parent WOULD broadacst data frames, if its furthest child were to disconnect
+    uint32_t own_pwr; // the minimum power with which the parent has to broadcast to reach us
 } parent_t;
 
+/**
+ * State of one of our children
+ */
 typedef struct {
     mac_addr_t addr;
-    uint32_t tx_pwr;
+    uint32_t tx_pwr; // minimum power with which we have to broadcast to reach this child
 } child_t;
 
+/**
+ * Our own state
+ */
 typedef struct {
-    child_t children[BREADTH];
-    uint32_t max_pwr;
-    uint32_t high_pwr;
-    uint32_t snd_high_pwr;
+    child_t children[BREADTH]; // list of currently connected children
+    uint32_t max_pwr; // maximum power at which we are able (or willing) to broadcast
+    uint32_t high_pwr; // the power at which we currently broadcast data frames
+    uint32_t snd_high_pwr; // // the power at which we WOULD broadacst data frames, if our furthest child were to disconnect
 } self_t;
 
+/**
+ * Track state of a currently pending connection
+ */
 typedef struct {
-    bool pending;
-    mac_addr_t parent;
-    uint32_t tx_pwr;
+    bool pending; // are we currently waiting for a response to a connection request?
+    mac_addr_t parent; // the parent to which we are attempting to connect
+    uint32_t tx_pwr; // power at which the parent has to broadcast to reach us (used later to update the parent's high_pwr)
 } pending_connection_t;
 
 #endif // __TREE_H__
