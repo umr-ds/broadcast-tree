1e879d5f5843d985fc1c85a75b8be766f8cb4e47
Artur Sterz <github@artursterz.de>
2021-09-02 12:07:11 +0200

    Make experiment params a little more nice

--- ns-3/experiment/experiment_params.sh
+++ ns-3/experiment/experiment_params.sh
@@ -1,7 +1,7 @@
-nWifi=(1 2 3 4 5) # Number of devices
-hopCount=(1 2 3 4 5) # The max hop count a broadcast message in SBP can travel
-eebtp=(true flase) # If EE-BTP or SBP should be used
-rndSeed=1000 # Seed for deterministic randomnes (e.g., where nodes are placed)
-iMax=30 # Number of simulations per parameter set
-cpm=(0 1 2) # The cycle prevention method to use
-size=(100 200 300 500) # Size of simulation area (size x size)
+NODES=(1 2 3 4 5)       # Number of devices
+HOP_COUNT=(1 2 3 4 5)   # The max hop count a broadcast message in SBP can travel
+EEBTP=(true flase)      # If EE-BTP or SBP should be used
+SEED=1000               # Seed for deterministic randomnes (e.g., where nodes are placed)
+RUNS=30                 # Number of simulations per parameter set
+CPM=(0 1 2)             # The cycle prevention method to use
+SIZE=(100 200 300 500)  # Size of simulation area (size x size)
