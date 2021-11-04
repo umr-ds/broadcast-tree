cf7770864ddc24a5bc3fca7ed7aad87ea2a22ff1
Artur Sterz <github2@artursterz.de>
2021-11-04 16:07:36 +0100

    [WIP] Pimod updates

--- pimod/firstboot.sh
+++ pimod/firstboot.sh
@@ -7,7 +7,7 @@ IFNAME="wlan0"
 MAC_ADDRESS="$(cat /sys/class/net/$IFNAME/address)"
 MIDDLE=$(echo "$MAC_ADDRESS" | cut -d':' -f5 | tr -d '[:space:]')
 SUFFIX=$(echo "$MAC_ADDRESS" | cut -d':' -f6 | tr -d '[:space:]')
-HNAME="btp-$SUFFIX"
+HNAME="btp-${MIDDLE}-${SUFFIX}"
 IP_ADDR="10.10.$(( 16#$MIDDLE )).$(( 16#$SUFFIX ))"
 
 # Set a unique hostname
@@ -15,20 +15,10 @@ echo "$HNAME" > /etc/hostname
 sed -i "s/raspberrypi/$HNAME/g" /etc/hosts
 hostname "$HNAME"
 
-# Install the Raspberry Pi linux sources
-rpi-source
-
 # Install the nexmon patch for BTP
-cp -r /btp/nexmon_patch /nexmon/patches/bcm43430a1/7_45_41_46/btp/
-cd /nexmon/patches/bcm43430a1/7_45_41_46/btp/ || exit 1
+cd /nexmon/patches/bcm43430a1/7_45_41_46/btp/
 source /nexmon/setup_env.sh
 make install-firmware
-
-# Make the nexmon installation permanent
-MODPATH=$(/sbin/modinfo | head -1 | cut -d':' -f2 | tr -d '[:space:]')
-cp "$MODPATH" /root/brcmfmac.ko.orig
-cp /nexmon/patches/bcm43430a1/7_45_41_46/btp/brcmfmac_kernel49/brcmfmac.ko "$MODPATH"
-depmod -a
 
 # Setup ad-hoc mode
 ifconfig "$IFNAME" down
